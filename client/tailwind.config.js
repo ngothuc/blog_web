8f0e61a7cbf6c02c41eaa7965c33e3cae298692c
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-07-08 01:16:16 +0700

    Create Routes and Pages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+    './public/index.html',
+  ],
+  darkMode: false,
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
-}
-
+};
